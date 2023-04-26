d7091719a4eb8b524d497f4f02434619cdf37fa0
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-04-26 16:21:35 +0200

    feat added show projects

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -6,12 +6,34 @@
 <div class="container">
     <div class="d-flex">
         <div >
-            <h1 class="title">{{$project->title}}</h1>
+            <h1 class="title">{{$project->title}}
+                @if($project->type)
+                <span class="badge rounded-pill bg-warning">{{ $project->type->name }}</span>
+                @else
+                    <span class="badge rounded-pill bg-secondary">Nessuna tipo trovato</span>
+                @endif
+        </h1></h1>
             <p class="slug">{{$project->slug}}</p>
             <p class="description">{{$project->description}}</p>
             <p class="date">{{$project->date}}</p>
             <p class="url">{{$project->url}}</p>
             
+        </div>
+        <div class="container">
+            <h2>Articoli correlati</h2>
+            @if($project->type)
+            <ul>
+                @foreach($project->type->projects as $type)
+                    <li>
+                        <a href="{{ route('projects.show',$type)}}">
+                            {{ $type->title }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+            @else
+                nessun articolo correlato
+            @endif
         </div>
         <div>
             <a class="btn" href="{{route('projects.edit',$project)}}">MODIFICA</a>
